1e8bce52c952612e2ef02912205a432b943b4f2b
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-12-02 19:52:07 -0300

    feat: create new activity

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,24 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnImc: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById<LinearLayout>(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+
+            startActivity(Intent(this, ImcActivity::class.java))
+
+
+        }
     }
 }
