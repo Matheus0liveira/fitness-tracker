d21e5998509702001bb6fe8eabf82fab5405d00c
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-12-02 22:19:13 -0300

    feat: remove keyboard after calculate imc

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,7 +1,10 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -36,8 +39,12 @@ class ImcActivity : AppCompatActivity() {
             AlertDialog.Builder(this)
                 .setTitle("Sei IMC é: ${getString(R.string.imc_response, result)}")
                 .setMessage(imcResponseId)
+                .setPositiveButton(android.R.string.ok) { _, _ -> }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
