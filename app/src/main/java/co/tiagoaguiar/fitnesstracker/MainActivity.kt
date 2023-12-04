3d2f70f62a98b54d000617901ef1aa11c57edab8
Matheus0liveiras <matheusfilho98.ms@gmail.com>
2023-12-04 19:57:52 -0300

    feat: create first recycle view

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,24 +1,51 @@
 package co.tiagoaguiar.fitnesstracker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnImc: LinearLayout
+    lateinit var recyclerViewMain: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById<LinearLayout>(R.id.btn_imc)
+        val adapter = MainAdapter()
+        recyclerViewMain = findViewById(R.id.rv_main)
+        recyclerViewMain.adapter = adapter
+        recyclerViewMain.layoutManager = LinearLayoutManager(this)
 
-        btnImc.setOnClickListener {
+//        btnImc = findViewById<LinearLayout>(R.id.btn_imc)
+//
+//        btnImc.setOnClickListener {
+//
+//            startActivity(Intent(this, ImcActivity::class.java))
+//
+//
+//        }
+    }
 
-            startActivity(Intent(this, ImcActivity::class.java))
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
+        }
 
+        override fun getItemCount(): Int {
+            return 15
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
 
         }
+    }
+
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
     }
 }
